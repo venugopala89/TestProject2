46848320e1dcf460d3cca8016b0a5ad1dafd8109
venugopala89 <venugopala.vtalent@gmail.com>
2019-12-20 10:50:00 +0530

    Business AccessLayer class created and one addition method added

---
+++ TestProject2/TestProject2/BusinessAccessLayer/Operation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProject2.BusinessAccessLayer
+{
+    public class Operation
+    {
+        public int Addition()
+        {
+            int a=10;
+            int b=10;
+            int c;
+            return c = a + b;
+
+        }
+    }
+}
